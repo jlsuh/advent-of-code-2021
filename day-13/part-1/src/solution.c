5392728b8bebb6b5d81321f899386efe8f5a1186
jlsuh <joelleonardosuh@gmail.com>
2022-01-03 08:20:53 -0300

    day-13

--- day-13/part-1/src/solution.c
+++ day-13/part-1/src/solution.c
@@ -4,20 +4,14 @@
 #include <string.h>
 
 #define FOLD_SIZE 18
-#define DIMS_SIZE 2
+#define DOTS_DELIMITER ','
 
 typedef struct {
-    int x;
-    int y;
-} t_point;
-
-typedef struct {
-    t_point* dots;
-    int numberOfDots;
     char** folds;
     int numberOfFolds;
-    t_point maxDims;
-    bool** presence;
+    bool** matrix;
+    int xMax;
+    int yMax;
 } t_instruction;
 
 void matrix_destroy(void** matrix, int dim) {
@@ -27,37 +21,28 @@ void matrix_destroy(void** matrix, int dim) {
     free(matrix);
 }
 
-bool** presence_create(t_instruction* instruction) {
-    bool** self = calloc(instruction->maxDims.y, sizeof(bool*));
-    for(int i = 0; i < instruction->maxDims.y; i++) {
-        self[i] = calloc(instruction->maxDims.x, sizeof(bool));
-    }
-    for(int i = 0; i < instruction->numberOfDots; i++) {
-        t_point dot = instruction->dots[i];
-        self[dot.y][dot.x] = true;
-    }
-    return self;
+void instruction_destroy(t_instruction* instruction) {
+    matrix_destroy((void**) instruction->folds, instruction->numberOfFolds);
+    matrix_destroy((void**) instruction->matrix, instruction->yMax);
+    free(instruction);
 }
 
 t_instruction* instruction_create(FILE* input, char dotDelimiter) {
     t_instruction* self = malloc(sizeof(t_instruction));
-    self->numberOfDots = 0;
+    int numberOfDots = 0;
     char c = '\0';
     while((c = fgetc(input)) != EOF) {
         if(c == dotDelimiter) {
-            self->numberOfDots++;
+            numberOfDots++;
         }
     }
     rewind(input);
-    t_point* dots = calloc(self->numberOfDots, sizeof(t_point));
     int xMax = -1;
     int yMax = -1;
-    for(int i = 0; i < self->numberOfDots; i++) {
+    for(int i = 0; i < numberOfDots; i++) {
         int x = -1;
         int y = -1;
         fscanf(input, "%d,%d", &x, &y);
-        dots[i].x = x;
-        dots[i].y = y;
         if(x > xMax) {
             xMax = x;
         }
@@ -65,12 +50,21 @@ t_instruction* instruction_create(FILE* input, char dotDelimiter) {
             yMax = y;
         }
     }
+    rewind(input);
+    self->xMax = xMax + 1;
+    self->yMax = yMax + 1;
+    self->matrix = calloc(self->yMax, sizeof(bool*));
+    for(int i = 0; i < self->yMax; i++) {
+        self->matrix[i] = calloc(self->xMax, sizeof(bool));
+    }
+    for(int i = 0; i < numberOfDots; i++) {
+        int x = -1;
+        int y = -1;
+        fscanf(input, "%d,%d", &x, &y);
+        self->matrix[y][x] = true;
+    }
     while((c = fgetc(input)) == '\n');
     ungetc(c, input);
-    self->maxDims.x = xMax + 1;
-    self->maxDims.y = yMax + 1;
-    self->dots = dots;
-    self->presence = presence_create(self);
     self->numberOfFolds = 0;
     self->folds = NULL;
     char* fold = calloc(FOLD_SIZE, sizeof(char));
@@ -88,185 +82,98 @@ t_instruction* instruction_create(FILE* input, char dotDelimiter) {
     return self;
 }
 
-void instruction_destroy(t_instruction* instruction) {
-    matrix_destroy((void**) instruction->presence, instruction->maxDims.y);
-    for(int i = 0; i < instruction->numberOfFolds; i++) {
-        free(instruction->folds[i]);
-    }
-    free(instruction->folds);
-    free(instruction->dots);
-    free(instruction);
-}
-
-void print_instruction(t_instruction* instruction) {
-    for(int i = 0; i < instruction->numberOfDots; i++) {
-        printf("(%d,%d) \n", instruction->dots[i].x, instruction->dots[i].y);
-    }
-    for(int i = 0; i < instruction->numberOfFolds; i++) {
-        printf("%s\n", instruction->folds[i]);
-    }
-    printf("MAXDIMS: (%d,%d) \n", instruction->maxDims.x, instruction->maxDims.y);
-}
-
-char** origami_create(t_instruction* instruction) {
-    char** self = calloc(instruction->maxDims.y, sizeof(char*));
-    for(int i = 0; i < instruction->maxDims.y; i++) {
-        self[i] = calloc(instruction->maxDims.x, sizeof(char));
-        memset(self[i], '.', instruction->maxDims.x);
-    }
-    for(int i = 0; i < instruction->numberOfDots; i++) {
-        t_point dot = instruction->dots[i];
-        self[dot.y][dot.x] = '#';
-    }
-    return self;
-}
-
-bool contains_point(t_instruction* instruction, int x, int y) {
-    return instruction->presence[y][x];
-}
-
-void print_origami(t_instruction* instruction) {
-    for(int j = 0; j < instruction->maxDims.y; j++) {
-        for(int i = 0; i < instruction->maxDims.x; i++) {
-            if(contains_point(instruction, i, j)) {
-                printf("#");
-            } else {
-                printf(".");
+void instruction_print(t_instruction* instruction) {
+    for(int i = 0; i < instruction->yMax; i++) {
+        for(int j = 0; j < instruction->xMax; j++) {
+            if(instruction->matrix[i][j]) {
+                printf("\e[1;33m#\e[0m");
+                continue;
             }
+            printf(".");
         }
         printf("\n");
     }
 }
 
-size_t set_main_dim_size(t_instruction* instruction, int axisValue, int upper, int dimLimit) {
-    size_t dimSize = 0;
-    int lower = 0;
-    for(int i = axisValue + 1; i < dimLimit; i++) {
-        lower++;
+void instruction_transpose(t_instruction* instruction) {
+    int tempYMax = instruction->yMax;
+    instruction->yMax = instruction->xMax;
+    instruction->xMax = tempYMax;
+    bool** matrix = calloc(instruction->yMax, sizeof(bool*));
+    for(int i = 0; i < instruction->yMax; i++) {
+        matrix[i] = calloc(instruction->xMax, sizeof(bool));
     }
-    if(upper >= lower) {
-        dimSize = upper;
-    } else {
-        dimSize = lower;
+    for(int i = 0; i < instruction->xMax; i++) {
+        for(int j = 0; j < instruction->yMax; j++) {
+            matrix[j][i] = instruction->matrix[i][j];
+        }
     }
-    return dimSize;
+    matrix_destroy((void**) instruction->matrix, tempYMax);
+    instruction->matrix = matrix;
 }
 
-t_instruction* fold_origami(t_instruction* instruction, int currFold) {
-    char axis = '\0';
+void instruction_fold(t_instruction* instruction, int currFold) {
+    char foldAxis = '\0';
     int axisValue = -1;
-    sscanf(instruction->folds[currFold], "%c=%d", &axis, &axisValue);
-    size_t yDimSize = -1;
-    size_t xDimSize = -1;
-    int upper = axisValue;
-    t_point* newDots = NULL;
-    int newNumberOfDots = 0;
-    int outerDimLimit = -1;
-    int innerDimLimit = -1;
-    if(axis == 'x') {
-        xDimSize = set_main_dim_size(instruction, axisValue, upper, instruction->maxDims.x);
-        yDimSize = instruction->maxDims.y;
-        outerDimLimit = instruction->maxDims.y;
-        innerDimLimit = instruction->maxDims.x - 1;
-    } else if(axis == 'y') {
-        yDimSize = set_main_dim_size(instruction, axisValue, upper, instruction->maxDims.y);
-        xDimSize = instruction->maxDims.x;
-        outerDimLimit = instruction->maxDims.x;
-        innerDimLimit = instruction->maxDims.y - 1;
+    sscanf(instruction->folds[currFold], "%c=%d", &foldAxis, &axisValue);
+    bool transposeAgain = false;
+    if(foldAxis == 'x') {
+        instruction_transpose(instruction);
+        transposeAgain = true;
     }
-    int destDim = 0;
-    for(int i = 0; i < outerDimLimit; i++) {
-        for(int j = innerDimLimit; j > axisValue; j--) {
-            int xSrc = -1;
-            int ySrc = -1;
-            int xDest = -1;
-            int yDest = -1;
-            if(outerDimLimit == instruction->maxDims.y) {
-                xSrc = j;
-                ySrc = i;
-                xDest = destDim;
-                yDest = ySrc;
-            } else if(outerDimLimit == instruction->maxDims.x) {
-                xSrc = i;
-                ySrc = j;
-                xDest = xSrc;
-                yDest = destDim;
-            }
-            if(contains_point(instruction, xSrc, ySrc)) {
-                if(!contains_point(instruction, xDest, yDest)) {
-                    instruction->dots = realloc(instruction->dots, ++(instruction->numberOfDots) * sizeof(t_point));
-                    instruction->dots[instruction->numberOfDots - 1].x = xDest;
-                    instruction->dots[instruction->numberOfDots - 1].y = yDest;
+    int upperIndex = axisValue - 1;
+    for(int col = 0; col < instruction->xMax; col++) {
+        for(int lowerIndex = axisValue + 1; lowerIndex < instruction->yMax; lowerIndex++) {
+            if(upperIndex < 0) {
+                instruction->yMax += 1;
+                bool** matrix = calloc(instruction->yMax, sizeof(bool*));
+                for(int newRowIndex = 0; newRowIndex < instruction->yMax; newRowIndex++) {
+                    matrix[newRowIndex] = calloc(instruction->xMax, sizeof(bool));
+                    if(newRowIndex > 0) {
+                        memcpy(matrix[newRowIndex], instruction->matrix[newRowIndex - 1], instruction->xMax * sizeof(bool));
+                    }
                 }
+                upperIndex = 0;
+                lowerIndex++;
+                axisValue++;
+                matrix_destroy((void**) instruction->matrix, instruction->yMax - 1);
+                instruction->matrix = matrix;
             }
-            destDim++;
+            if(instruction->matrix[lowerIndex][col]) {
+                instruction->matrix[upperIndex][col] = instruction->matrix[lowerIndex][col];
+            }
+            upperIndex--;
         }
-        destDim = 0;
+        upperIndex = axisValue - 1;
     }
-    for(int i = 0; i < instruction->numberOfDots; i++) {
-        int limit = -1;
-        if(innerDimLimit == instruction->maxDims.x - 1) {
-            limit = instruction->dots[i].x;
-        } else if(innerDimLimit == instruction->maxDims.y - 1) {
-            limit = instruction->dots[i].y;
-        }
-        if(limit < axisValue) {
-            newDots = realloc(newDots, ++newNumberOfDots * sizeof(t_point));
-            newDots[newNumberOfDots - 1].x = instruction->dots[i].x;
-            newDots[newNumberOfDots - 1].y = instruction->dots[i].y;
+    for(int i = instruction->yMax - 1; i >= axisValue; i--) {
+        free(instruction->matrix[i]);
+    }
+    instruction->yMax = axisValue;
+    if(transposeAgain) {
+        instruction_transpose(instruction);
+    }
+}
+
+int number_of_dots(t_instruction* instruction) {
+    int dots = 0;
+    for(int i = 0; i < instruction->yMax; i++) {
+        for(int j = 0; j < instruction->xMax; j++) {
+            if(instruction->matrix[i][j]) {
+                dots++;
+            }
         }
     }
-    free(instruction->dots);
-    instruction->dots = newDots;
-    instruction->numberOfDots = newNumberOfDots;
-    matrix_destroy((void**) instruction->presence, instruction->maxDims.y);
-    instruction->maxDims.x = xDimSize;
-    instruction->maxDims.y = yDimSize;
-    instruction->presence = presence_create(instruction);
-//     if(++currFold <= instruction->numberOfFolds) {
-//         origami = fold_origami(newOrigami, instruction, currFold);
-//     }
-//     print_origami(instruction);
-    return instruction;
+    return dots;
 }
 
 int solution(FILE* input) {
-    t_instruction* instruction = instruction_create(input, ',');
-    print_instruction(instruction);
-
-//     for(int i = 0; i < instruction->numberOfFolds; i++) {
-//         instruction = fold_origami(instruction, 0);
-//     }
-
-    instruction = fold_origami(instruction, 0);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 1);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 2);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 3);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 4);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 5);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 6);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 7);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 8);
-    print_origami(instruction);
-    instruction = fold_origami(instruction, 9);
-    print_origami(instruction);
-    printf("\n");
-    instruction = fold_origami(instruction, 10);
-    print_origami(instruction);
-    printf("\n");
-    instruction = fold_origami(instruction, 11);
-    print_origami(instruction);
-    
-    int numberOfDots = instruction->numberOfDots;
-
+    t_instruction* instruction = instruction_create(input, DOTS_DELIMITER);
+    for(int i = 0; i < instruction->numberOfFolds; i++) {
+        instruction_fold(instruction, i);
+    }
+    instruction_print(instruction);
+    int numberOfDots = number_of_dots(instruction);
     instruction_destroy(instruction);
     return numberOfDots;
 }
@@ -283,12 +190,3 @@ int main(int argc, char *argv[] /*ARGS="../input.txt"*/) {
     fclose(input);
     return 0;
 }
-
-/*
-####.####.####.#..#.####.####...##.#....
-####.####.####.#.#..#.##.####.#.##.#....
-#.#..#....####.##.#.###..####..###..###.
-####.###..##.#.#.##.##...####...##.#....
-#....#..#.####.#.##.####.##.#.####.##...
-#....#..#.####.####.####.####.####.####.
-*/
